c25361f07bda05bd82e2f9ddbd133619a3368b04
Haggai Lazaro <haggai@Haggais-MBP.hitronhub.home>
2018-04-09 04:47:51 -0600

    debugging single company

--- src/containers/SingleCompany.js
+++ src/containers/SingleCompany.js
@@ -6,7 +6,7 @@ class SingleCompany extends Component {
     constructor(props) {
         super(props);
         this.state={
-            foundData: 0,
+            foundCompany: 0,
             path: ["Home", "Companies", "Single Company"]
         }
     }
